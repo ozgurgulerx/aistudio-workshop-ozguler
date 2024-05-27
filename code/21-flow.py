a1cbba832984a327f4245aadea03acc5bf37d506
Ozgur Guler <ozgurguler@MESA-X-2.local>
2024-05-27 16:15:10 +0300

    iteration number codefirst16

--- code/21-flow.py
+++ code/21-flow.py
@@ -1,26 +1,3 @@
-import os
-
-from dotenv import load_dotenv
-from pathlib import Path
-from promptflow.tracing import trace
-from promptflow.core import Prompty
-
-BASE_DIR = Path(__file__).absolute().parent
-
-
-@trace
-def chat(question: str = "What's the capital of France?") -> str:
-    """Flow entry function."""
-
-    if "OPENAI_API_KEY" not in os.environ and "AZURE_OPENAI_API_KEY" not in os.environ:
-        # load environment variables from .env file
-        load_dotenv()
-
-    prompty = Prompty.load(source=BASE_DIR / "21-chat.prompty")
-    output = prompty(question=question)
-    return output
-
-
 if __name__ == "__main__":
     from promptflow.tracing import start_trace
 
