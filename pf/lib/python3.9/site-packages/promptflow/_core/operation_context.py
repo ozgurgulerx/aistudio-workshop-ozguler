fac27d24c675445381e7709db54cd1072711495f
OZGUR GULER <97877847+ozgurgulerx@users.noreply.github.com>
2024-05-27 15:39:08 +0300

    Delete pf directory

--- pf/lib/python3.9/site-packages/promptflow/_core/operation_context.py
+++
@@ -1,3 +0,0 @@
-# flake8: noqa
-# for backward compatibility of PRS
-from promptflow.tracing._operation_context import OperationContext
--- pf/lib/python3.9/site-packages/promptflow/executor/__init__.py
+++
@@ -1,7 +0,0 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
-
-# flake8: noqa
-from .flow_executor import FlowExecutor
-from .flow_validator import FlowValidator
