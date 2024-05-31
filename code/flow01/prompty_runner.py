2931e4c28413260574f10c8100652535d4c0ffff
Ozgur Guler <ozgurguler@MESA-X-2.local>
2024-05-31 09:11:55 +0300

    iteration number codefirst 240529-01

--- code/flow01/prompty_runner.py
+++ code/flow01/prompty_runner.py
@@ -1,14 +1,24 @@
 from promptflow.core import Prompty
+from promptflow.core import tool
+from dotenv import load_dotenv
+
 import os
 
-
-def run_prompty(theme):
+@tool
+def run_prompty_tool(theme: str) -> str:
     # Get the directory of this script
     current_dir = os.path.dirname(os.path.abspath(__file__))
+    # Adjust the path to point to the flow01 directory
     prompty_file_path = os.path.join(current_dir, "creative_writing_assistant.prompty")
+    print(prompty_file_path)
     
+    if "OPENAI_API_KEY" not in os.environ and "AZURE_OPENAI_API_KEY" not in os.environ:
+    # load environment variables from .env file
+        load_dotenv()
+
     # Load prompty as a flow
     prompty_flow = Prompty.load(prompty_file_path)
+    
     
     # Execute the flow as a function
     result = prompty_flow(theme=theme)
